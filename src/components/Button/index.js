7e608832b1920ab3419ac997df99f8bb46919ed4
Francisco Murillo <fmurillodev@gmail.com>
2021-10-19 11:03:52 -0300

    create button component and css styles to use in add and update class

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,5 @@
-const Button = () => {
-  //TODO: create Button
-  return <div>Button</div>;
+const Button = ({ className, label }) => {
+  return <button className={className}>{label}</button>;
 };
 
 export default Button;
