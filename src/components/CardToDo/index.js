fac718b73fca25161cbf1ecab28af9ec0dbb4ec0
Francisco Murillo <fmurillodev@gmail.com>
2021-10-19 13:25:54 -0300

    create CardToDo component, install react-icons, and add css styles

--- src/components/CardToDo/index.js
+++ src/components/CardToDo/index.js
@@ -1,6 +1,29 @@
-const CardTodo = () => {
-  // TODO: create cardToDo Component
-  return <div>Card To Do</div>;
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
+
+const CardTodo = ({
+  id,
+  label,
+  completeTodo,
+  className,
+  removeTodo,
+  setEdit,
+}) => {
+  return (
+    <div className={className}>
+      <div onClick={() => completeTodo(id)}>{label}</div>
+      <div className="icons">
+        <RiCloseCircleLine
+          onClick={() => removeTodo(id)}
+          className="delete-icon"
+        />
+        <TiEdit
+          className="edit-icon"
+          onClick={() => setEdit({ id, value: label })}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CardTodo;
