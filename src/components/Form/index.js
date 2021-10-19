052fbb157fe45028a0fcceecddc23843a7072d00
Francisco Murillo <fmurillodev@gmail.com>
2021-10-19 12:43:09 -0300

    create Form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,32 @@
-const Form = () => {
-  // TODO: create ToDo Form
-  return <div>Form ToDo</div>;
+import React, { useState } from "react";
+import Input from "../Input";
+import Button from "../Button";
+
+const Form = ({ onSubmit, placeholder, classNameButton, label, edit }) => {
+  const [input, setInput] = useState(edit?.value || "");
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(input);
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        placeholder={placeholder}
+        className="todo-input"
+        onChange={handleChange}
+        value={input}
+      />
+      <Button className={classNameButton} label={label} />
+    </form>
+  );
 };
 
 export default Form;
